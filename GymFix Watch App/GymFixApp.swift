9752c8b8200632e41e76131211ce1c4add1c2979
Hans Jonathan Zebua <hans.zebua@icloud.com>
2024-05-21 13:43:01 +0700

    Initial Commit

---
+++ GymFix Watch App/GymFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFixApp.swift
+//  GymFix Watch App
+//
+//  Created by Hans Zebua on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct GymFix_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
